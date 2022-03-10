886c4e5ada8dec1296222df393b630b4ee64dad5
Samzee4 <79870129+zayassam@users.noreply.github.com>
2022-03-10 19:25:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+/*Eric Lalande
+  Marta Polak
+  Tsedenya Melaku
+  Todd Skahill
+  Sam Zayas
+  Simegne Wordofa*/
+
+// Equations to look at for problem
+let totalBags = 1000;
+let BagsSold = 0;
+bagsInStock = totalBags - bagsSold
+bagToDeliver = totalBags - bagsInStock
+candyNeeded =totalNumberOfCandyBags- totalNumberOfCandyBagsold 
+employeeNeeded = candyNeeded / 200
+costOfDelivery = (25*employeeNeeded) + (candyNeeded*3). 
+
+
+
+
+switch(bagToDeliver)
+  case bagToDeliver===800 && bagToDeliver<1000 :
+   totalNumberOfEmployee=bagToDeliver/200;
+      costOfDelivery = (25*totalNumberOfEmployee) + (totalNumberOfCandyBagsold*3);
+      break;
+  case (bagToDeliver===600 && bagToDeliver<800):
+   totalNumberOfEmployee=bagToDeliver/200;
+      costOfDelivery = (25*totalNumberOfEmployee) + (totalNumberOfCandyBagsold*3);
+      break;
+  case (bagToDeliver===400 && bagToDeliver<600):
+   totalNumberOfEmployee=bagToDeliver/200;
+      costOfDelivery = (25*totalNumberOfEmployee) + (totalNumberOfCandyBagsold*3);
+      break;
+  case (bagToDeliver===200 && bagToDeliver<400):
+   totalNumberOfEmployee=bagToDeliver/200;
+      costOfDelivery = (25*totalNumberOfEmployee) + (totalNumberOfCandyBagsold*3);
+      break;
+costOfDelivery = (25*totalNumberOfEmployee) + (totalNumberOfCandyBagsold*3);
+console.log( 'needed'+costOfDelivery+totalNumberOfEmployee);
+
+
+//Console will display text
+console.log(`JWD SWeets ha ${bagsInStock} JWD Bites remaining`);
+console.log(`${bagsSold} are needed!`);
+console.log(` JWD Sweets needs ${employeeNeeded}`);
+console.log(`They total cost for this delivery will be ${totalCostOfDelivery}`);
+console.log(`Have A \"Sweet Day\"`);
+      
